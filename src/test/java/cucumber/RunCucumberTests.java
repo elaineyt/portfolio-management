80c911ac4611195c06f3f2424e4418a39c6874b4
Andreas Bigger <abigger87@gmail.com>
2020-11-05 19:23:40 -0800

    add user for failing andreas tests

--- src/test/java/cucumber/RunCucumberTests.java
+++ src/test/java/cucumber/RunCucumberTests.java
@@ -11,20 +11,20 @@ import org.junit.runner.RunWith;
  */
 @RunWith(Cucumber.class)
 @CucumberOptions(strict = true, features = {
-		//"src/test/resources/cucumber/graphHistorical.feature",
-		//"src/test/resources/cucumber/graphPortfolio.feature",
-		//"src/test/resources/cucumber/graphA.feature",
-		//"src/test/resources/cucumber/mobile.feature",
-		//"src/test/resources/cucumber/SSL.feature",
-		//"src/test/resources/cucumber/zoom.feature",
-		//"src/test/resources/cucumber/graphDates.feature",
-		//"src/test/resources/cucumber/graphH.feature"
+		"src/test/resources/cucumber/graphHistorical.feature",
+		"src/test/resources/cucumber/graphPortfolio.feature",
+		"src/test/resources/cucumber/graphA.feature",
+		"src/test/resources/cucumber/mobile.feature",
+		"src/test/resources/cucumber/SSL.feature",
+		"src/test/resources/cucumber/zoom.feature",
+		"src/test/resources/cucumber/graphDates.feature",
+		"src/test/resources/cucumber/graphH.feature",
+		"src/test/resources/cucumber/graphB.feature"
 		//"src/test/resources/cucumber/LimitedLoginAttempts.feature", 
 		// "src/test/resources/cucumber/graphH.feature",
 		//"src/test/resources/cucumber/historical.feature",
 		//"src/test/resources/cucumber/parser.feature",
 		//"src/test/resources/cucumber/mainpage.feature",
-		"src/test/resources/cucumber/graphB.feature"
 		})
 public class RunCucumberTests {
 
