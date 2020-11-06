c75d75e2ac496558692a9d3808450e900288e2af
Andreas Bigger <abigger87@gmail.com>
2020-11-05 20:04:23 -0800

    add color to run cucumber tests

--- src/test/java/cucumber/RunCucumberTests.java
+++ src/test/java/cucumber/RunCucumberTests.java
@@ -20,7 +20,8 @@ import org.junit.runner.RunWith;
 		"src/test/resources/cucumber/zoom.feature",
 		"src/test/resources/cucumber/graphDates.feature",
 		"src/test/resources/cucumber/graphH.feature",
-		"src/test/resources/cucumber/graphB.feature"
+		"src/test/resources/cucumber/graphB.feature",
+		"src/test/resources/cucumber/graphColor.feature"
 		//"src/test/resources/cucumber/LimitedLoginAttempts.feature", 
 		// "src/test/resources/cucumber/graphH.feature",
 		//"src/test/resources/cucumber/historical.feature",
